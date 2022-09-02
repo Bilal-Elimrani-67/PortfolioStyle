6293a92c95504f881c702b2e4290977f9bcb14b0
bilal <bilal@gmail.com>
2022-09-02 16:09:32 +0200

    Scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,5 +5,13 @@ const scroll = new LocomotiveScroll({
   smartphone: { smooth: true },
 });
 
-// BODY COLOR //
-scroll.on("scroll");
+// Body color
+scroll.on("scroll", () => {
+  if (document.querySelector("#color.is-inview")) {
+    document.body.style.background = "#000101";
+    document.body.style.color = "#fefeff";
+  } else {
+    document.body.style.background = "#fefeff";
+    document.body.style.color = "#000101";
+  }
+});
