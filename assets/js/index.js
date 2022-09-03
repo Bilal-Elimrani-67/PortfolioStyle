b5461451cc79a75ca1f9b359137bfd792f692327
bilal <bilal@gmail.com>
2022-09-03 18:01:55 +0200

    Ajout de mes projets

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,13 @@ const scroll = new LocomotiveScroll({
   smartphone: { smooth: true },
 });
 
-// Body color
+// BODY COLOR //
 scroll.on("scroll", () => {
   if (document.querySelector("#color.is-inview")) {
-    document.body.style.background = "#000101";
-    document.body.style.color = "#fefeff";
+    document.body.style.background = "#c684b5";
+    document.body.style.color = "#D2D2EB";
   } else {
-    document.body.style.background = "#fefeff";
-    document.body.style.color = "#000101";
+    document.body.style.background = "#D2D2EB";
+    document.body.style.color = "#c684b5";
   }
 });
